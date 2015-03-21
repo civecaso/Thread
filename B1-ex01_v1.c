8f911d1df669b3bdd2506615ad604b1d64fb832c
DavideCasonato <davide_casonato@libero.it>
2015-03-21 12:12:58 +0100

    Aggiornamento versione 2.0

--- B1-ex01_v1.c
+++ B1-ex01_v1.c
@@ -1,9 +1,16 @@
 /*
-   v1 - Prima versione Produttore-Consumatore.
-   Non abbiamo ALTERNANZA STRETTA
-   come si vede dall'esecuzione i programmi
-   hanno velocità diverse e quindi l'output
-   non è corretto.
+   v2 - Produttore e Consumatore.
+   Introduco delle variabili BOOLEAN
+   che mi permettono di capire se
+   il dato è stato letto o scritto:
+   buffer empty (letto) e buffer
+   full (scritto); ora ottengo
+   la stretta alternanza: P/C/P/C...
+   
+   In questo esempio la variabile condivisa
+   full non è protetta da una sezione
+   critica!
+   
 */
 
 #include <stdio.h>
@@ -12,53 +19,134 @@
 #include <time.h>
 #define N 100
 
-int buffer; //buffer di scambio dati fra P e C
+int buffer;
+int full; //buffer pieno 
+
+unsigned long WINAPI Produce_thread(void* arg);
+unsigned long WINAPI Consume_thread(void* arg);
 int RandomNumber(int from, int to);
-unsigned long WINAPI Produce_thread(void* arg); // Produttore
-unsigned long WINAPI Consume_thread(void* arg); // Consumatore
 
 int main()
-{   HANDLE P_thread, C_thread;
-    int low, high,count;
-    low=1;high=6;
+{   int low, high;
+    HANDLE P_thread, C_thread;
+    
+	low=1;   high=6;
 	srand(time(NULL));
 	printf("Produttore e Consumatore WIN\n");
-	printf("test phase for random numer from %d to %d:\n",low,high);
-	for(count=0;count<100;count++)  printf("%d ", RandomNumber(low,high));
-	while(!kbhit());	//aspetto che venga premuto un tasto
-	printf("We have 2 thread one produce the other consume\n");
-	delay(2000);
-	clrscr();
-    // creazione dei thread 
+	printf("We have 2 thread one produce other read from buffer\n");	
+
+    full=FALSE; //dato non pronto
+    
+    // creazione dei thread corrispondenti alle funzioni di lettura, elaborazione e scrittura
     P_thread = CreateThread(NULL, 1024, &Produce_thread, NULL, 0, NULL);
     C_thread = CreateThread(NULL, 1024, &Consume_thread, NULL, 0, NULL);
     // attesa della terminazione dei thread
     WaitForSingleObject(P_thread, INFINITE);
     WaitForSingleObject(C_thread, INFINITE);
+    printf("\n...END\n");
     return 0;
 }
 unsigned long WINAPI Produce_thread(void* arg) // Produttore
-{	int r,i;
+{	int r; int i;
 	for(i=0;i<100;i++){
-	    r=RandomNumber(1,6);
-	    printf("-P:%d",r);
-	    delay(RandomNumber(1,5)*10);
-	    buffer=r; //scrivo sul buffer
-	}
+	    r=RandomNumber(1,6);	    	    
+        while(full) printf("<P>"); //busy waiting 
+        //delay(RandomNumber(1,3)*10);
+		buffer=r; //scrivo sul buffer
+		printf("-P:%d",r);
+		full=TRUE; //segnalo anche che ora il buffer è pieno
+		}
 	ExitThread(0);
 }
+
 unsigned long WINAPI Consume_thread(void* arg) // Consumatore
-{
-	int r,i;
+{	int r; int i;
 	for(i=0;i<100;i++){
-	    delay(RandomNumber(1,5)*5);
+		//delay(RandomNumber(1,3)*10);    
+        while(!full) printf("<V>"); //busy waiting 
 		r=buffer;//leggo dal buffer
-	    printf("-C:%d\n",r);
-	    }
+        printf("-C:%d\n",r);
+		full=FALSE;//segnalo che ho letto il dato 
+		}
 	ExitThread(0);
 }
+
+
+
 int RandomNumber(int from, int to)
 {   int r;
+
     r=rand()%(to-from+1)+from;
+	
 	return r;
 }
+
+/*N=100000;
+thread P()
+
+thred C()
+
+main()
+{
+   empty=TRUE;
+   full=FALSE;
+   count=0;
+   esegui il thread P
+   esegui il thread C
+   aspetta la fine di P
+   aspetta la fine di C
+   return 0;
+}
+
+thread P()
+ total=1000;
+ do
+{ 
+
+  if (full) sleep (not_full);
+   1. full falsa
+     2. full vera ho dormito
+            e poi qualcuno mi
+            ha risvegliato.
+  
+  r=random;
+  total--
+  delay(variabile fra 0.1 e 5 sec)
+  b[i];
+  i++;
+  count++;
+  if !(count<N) full=TRUE;
+  if (empty==TRUE) 
+     empty=FALSE;
+     wake(not_empty)
+
+  if total==0 continua=FALSE
+ }while(continua) 
+  
+ if(empty==TRUE)
+    wake(not_empty)
+  exit
+ 
+
+
+thred C()
+
+ do{
+   if(empty) sleep(not_empty)
+   stampa b[j]
+   delay(variabile fra 0.1 e 5 sec)
+   j++
+   count--
+   if (count==0) 
+      empty=TRUE
+      if continua==FALSE
+         consumatore fuori!
+   if (full==TRUE) 
+       full=FALSE   
+       wake(not_full)
+  }while(1)
+
+*/
+
+
+	
